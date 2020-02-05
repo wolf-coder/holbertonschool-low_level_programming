9d67028541124ee10523dd870fa576008e8edefd
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-02-05 22:26:33 +0100

    Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 /**
  * main - main block
  * Return: 0
  */
 int main(void)
 {
-	int i=0;
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	while(str[i]!='\0'){
-		putchar(str[i]);
-		i++;
-	}
-	putchar('\n');
-	return (1);
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, str, strlen(str));
+return (1);
 }
