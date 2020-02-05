873f428e55fa940d382658b888136d28ee4c21c0
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-02-05 13:41:45 +0100

    updating  /holberton-system_engineering-devops/0x01-shell_permissions

---
+++ update_Repository.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#do not forget to put this script name in the .gitignore directory in order to not 'push' it  !! -> you can execute this commands:
+# echo update_Repository.sh  >>.gitignore
+
+#git clone https://username:password@github.com/username/repository.git
+ git add . && git commit -m "updating  /holberton-system_engineering-devops/0x01-shell_permissions"  && git push
