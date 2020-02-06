a22ef337a0e0c28b67c09629fdef3302587bb72e
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-02-06 20:06:30 +0100

    Prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- Prints the alphabet in lowercase, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	for (char i = 'a'; i <= 'z'; ++i)
+	{
+		putchar(i);
+	}
+}
