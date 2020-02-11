3db478e98eaa90eac24bfa1c647dd587835afee4
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-02-11 12:49:36 +0100

    A unction that computes the absolute value of an integer.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _abs - A  function that  computes the absolute value of an integer.
+ *in lowercase, followed by a new line
+ *@n: The number to compute ist absolute value.
+ * Return: If alpha 1 ,otherwise 0
+ **/
+int _abs(int n)
+{
+if (n >= 0)
+{
+	return (n);
+} else
+{
+	return (-n);
+}
+}
