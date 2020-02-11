a5d025fccce3aaef3496a76f33303695941ea28e
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-02-11 22:50:43 +0100

    print_to_98 - A function that prints all natural numbers from n.

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - A function that prints all natural numbers from n
+ *to 98, followed by a new line.
+ *
+ *@n: Delimater number
+ *Return: void
+ **/
+void print_to_98(int n)
+{
+n = 111;
+int i = 0;
+printf("%d", n);
+if (n <= 98)
+{
+for (i = n + 1; i <= 98; ++i)
+{
+printf(", %d", i);
+}
+}
+else
+{
+for (i = n - 1; i > 98; --i)
+{
+printf(", %d", i);
+}
+}
+printf("\n");
+}
