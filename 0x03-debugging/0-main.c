aba288016f1f61268e351e26d743ca37a4041337
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-02-11 23:49:21 +0100

    * main - tests function that prints if integer is positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+int main(void)
+{
+int j;
+j = 0;
+positive_or_negative(j);
+return (0);
+}
