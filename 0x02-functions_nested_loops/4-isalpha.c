21e55ff69785aeb8f7a37863d95ce520e753e667
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-02-11 12:18:12 +0100

    a function that checks for alphabetic character.

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _islower - A  function that checks for alphabetic character.
+ *in lowercase, followed by a new line
+ *@c: The character to check whether it is alphabetic.
+ * Return: If alpha 1 ,otherwise 0
+ **/
+int _isalpha(int c)
+{
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+{
+	return (1);
+} else
+{
+	return (0);
+}
+}
