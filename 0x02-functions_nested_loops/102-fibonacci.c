300ec7a0d9520e2ed4203c3bf0c1fdcab7576e0e
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-02-12 23:11:42 +0100

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - computes and prints the
+*sum of all the multiples of 3 or 5 below 1024
+* Return: 0
+*/
+int main(void)
+{
+long int m = 1, k = 2, i = 0;
+long int sum = 0;
+printf("%ld, %ld, ", m, k);
+for (i = 0; i < 47; ++i)
+{
+sum = m + k;
+printf("%ld, ", sum);
+m = k;
+k = sum;
+}
+sum = m + k;
+printf("%ld\n", sum);
+return (0);
+}
