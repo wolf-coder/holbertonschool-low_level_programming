130230e4f5dfef9d40e51c7a96bba152d344ab20
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-02-13 20:49:58 +0100

    Fizz-Buzz program.

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+* main - Fizz-Buzz test
+*
+* Return: 0 success.
+*/
+int main(void)
+{
+	int i = 0;
+
+	for (i = 1; i <= 100; ++i)
+	{
+		if (i % 3 == 0)
+		{
+			if (i % 5 == 0)
+			{
+				printf("FizzBuzz");
+			} else
+			{
+				printf("Fizz");
+			}
+		} else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i != 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
