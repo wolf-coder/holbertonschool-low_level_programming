61a2351020573fddf990db80efa10c53b2ad1ede
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-02-13 12:32:01 +0100

    Write a function that checks for a digit (0 through 9).

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
-* _isdigit -  checks for uppercase character.
+*_isdigit -  checks for uppercase character.
 *@c: the character to check whether it is digital or not.
 * Return: 1 if c is digital , 0 otherwise.
-*/
+**/
 int _isdigit(int c)
 {
 if (c >= 48 && c <= 57)
