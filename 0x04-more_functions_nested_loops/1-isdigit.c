2b81f248e378babd94f400c1338504ab9c4d81ba
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-02-13 11:53:21 +0100

    checks for uppercase character.

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* _isdigit -  checks for uppercase character.
+*@c: the character to check whether it is digital or not.
+* Return: 1 if c is digital , 0 otherwise.
+*/
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
