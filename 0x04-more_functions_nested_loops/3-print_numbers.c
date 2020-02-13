f0fd8e348bd01d169664e751b6f21f5a3ae0ac3d
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-02-13 12:46:33 +0100

    * mul - print numbers [0..9]

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* mul - print numbers [0..9]
+*
+* Return: Void.
+*/
+void print_numbers(void)
+{
+int i;
+for (i = '0'; i <= '9'; ++i)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
