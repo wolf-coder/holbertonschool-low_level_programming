9bc2ede853be055137f0fe92773f693bd635fb18
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-02-18 23:39:37 +0100

    fixing betty Styling.

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,7 +13,7 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		if (i == n - 1)
-			printf("%d", *(a + n -1 ));
+			printf("%d", *(a + n - 1));
 		else
 			printf("%d, ", *(a + i));
 	}
