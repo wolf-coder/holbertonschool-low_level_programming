d0633218e192539c7ff2f551b2db9396441e9e54
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-02-18 19:10:32 +0100

    fixing betty Styling.

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,14 @@
 /**
 *_strlen - function that that returns the length of a string.
 *
-*@s : pointer of char type 
-*return - The length of a string
+*@s: pointer of char type.
+*Return: - The length of a string.
 */
 int _strlen(char *s)
 {
 	int i = 0;
-	while(s[i] != '\0')
+
+	while (s[i] != '\0')
 		i++;
 	return (i);
 }
