4dbc1311fae8b05f5f5b5fef9a74482976e1dd64
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-02-20 16:32:31 +0100

    A function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+*_strncat - A function that concatenates two strings.
+*
+*@dest: Pointer to the array.
+*@src: Pointer to the array.
+*@n: number of bytes to copy.
+*Return: Pointer to the resulting string dest.
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int lenDest = _strlen(dest);
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[lenDest + i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+/**
+* _strlen - function that that returns the length of a string.
+* @s : pointer to a string.
+* Return: string lenght
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
