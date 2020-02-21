50e1947a2cdffc23db75e473afd9c773686b5f3b
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-02-21 19:58:05 +0100

    *leet - A function that encodes a string into 1337.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,31 +9,18 @@
 char *leet(char *str)
 {
 	int i, j;
-	char c1[] = "aAeEoOtTlL";
-	char c2[] = "43071";
+	char c1[] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
+	char c2[] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
 
 	for (i = 0; str[i] != '\0'; ++i)
 	{
-		for (j = 0; c1[j] != '\0'; ++j)
+		for (j = c1[j] != '\0'; ++j)
 		{
 			if (str[i] == c1[j])
 			{
-				str[i] = c2[j / 2];
+				str[i] = c2[j];
 			}
 		}
 	}
 	return (str);
-}
-/**
-* _strlen - function that that returns the length of a string.
-* @s : pointer to a string.
-* Return: string lenght
-*/
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		i++;
-	return (i);
 }
