62bf802d9eeae048b1e0f0cd690b25317fd44f44
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-02-21 20:01:33 +0100

    *leet - A function that encodes a string into 1337.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,22 +6,22 @@
 *
 *Return: string pointer.
 */
-char *leet(char *v)
+char *leet(char *str)
 {
 	int a = 0, b;
 	char c1[] = {'a', 'e', 'o', 't', 'l'};
 	char c2[] = {'4', '3', '0', '7', '1'};
 
-	while (v[a] != '\0')
+	while (str[a] != '\0')
 	{
 		for (b = 0; c1[b] != '\0'; b++)
 		{
-			if (v[a] == c1[b] || v[a] == c1[b] - 32)
+			if (str[a] == c1[b] || str[a] == c1[b] - 32)
 			{
-				v[a] = c2[b];
+				str[a] = c2[b];
 			}
 		}
 		a++;
 	}
-	return (v);
+	return (str);
 }
