e28cc481e0f07f26e28ecd1345c1cc69e1235f52
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-02-24 14:47:14 +0100

    a function that locates a character in a string.

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stddef.h>
+/**
+*_strchr -  This function that that locates a character in a string.
+*overwriting the terminating null byte (\0) at the end of dest, and then adds a
+*terminating null byte.
+*
+*@s: Pointer to the array.
+*@c: A char byte to look for.
+*Return: Returns a pointer to the first occurrence of the character c in the
+*string s, or NULL if the character is not found
+*/
+char *_strchr(char *s, char c)
+{
+	char *p = s;
+
+	while (*p != '\0')
+	{
+		if (*p == c)
+		{
+			return (p);
+		}
+		p++;
+	}
+	if (*p == '\0')
+	{
+		p = NULL;
+	}
+	return (NULL);
+}
+
