86082bedb220d7b036aec34fce93e4006c4d41b1
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-02-25 21:09:18 +0100

    Adding print_chessboard() prototype.

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,6 +1,7 @@
 #ifndef GRANDPARENT_H
 #define GRANDPARENT_H
 
+int _putchar(char c);
 int _strlen(char *s);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -8,4 +9,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
 #endif
