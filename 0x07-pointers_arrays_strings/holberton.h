4aaa51dcdbdfe24cbfc1a226ecccb8462f7f6c52
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-02-25 01:56:14 +0100

    Adding strstr() prototype.

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,9 +1,11 @@
 #ifndef GRANDPARENT_H
 #define GRANDPARENT_H
 
+int _strlen(char *s);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 #endif
