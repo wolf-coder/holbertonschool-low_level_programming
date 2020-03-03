7c89273de2de909ee2dd7cfc9f8f098774d10373
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-03-03 20:13:08 +0100

    fixing output.

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,21 +2,6 @@
 #include <stdlib.h>
 
 /**
-*_strlen - function that that returns the length of a string.
-*
-*@s: pointer of char type.
-*Return: - The length of a string.
-*/
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-
-/**
  * _strdup - function that returns a pointer to a newly allocated space in
  *  memory, which contains a copy of the string given as a parameter.
  *@str : array.
@@ -26,11 +11,14 @@ int _strlen(char *s)
 
 char *_strdup(char *str)
 {
-	int i = 0, len = _strlen(str);
+	int i = 0, len = 0;
 	char *p;
 
 	if (str == NULL)
 		return (NULL);
+	while(str[len] != '\0' )
+		len++;
+	len++;
 	p = malloc(len * sizeof(char));
 	if (p == NULL)
 		return (NULL);
