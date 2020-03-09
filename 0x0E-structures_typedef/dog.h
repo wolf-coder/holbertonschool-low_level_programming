02270b98e8c272552eab274e99b58df1deac5527
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-03-09 23:59:45 +0100

    Commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Info of the dog
+ * @name: Dog name
+ * @age: age
+ * @owner: Owner
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
