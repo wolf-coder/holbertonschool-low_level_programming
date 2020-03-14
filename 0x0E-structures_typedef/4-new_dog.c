02654209d268629b319b57e8d5f4a984ea9add24
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-03-14 19:39:42 +0100

    fixing output.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,33 @@
 #include "dog.h"
 #include <stdlib.h>
+#define SIZE(x) ((sizeof(x) / sizeof(x[0])))
+
+/**
+ * _strdup - function that returns a pointer to a newly allocated space in
+ *  memory, which contains a copy of the string given as a parameter.
+ *@str : array.
+ * Return: pointer to a new string , NULL if str = NULL , on success eturns a
+ * pointer to the duplicated string
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0, len = 0;
+	char *p;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[len] != '\0')
+		len++;
+	len++;
+	p = malloc(len * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0 ; i <= len ; i++)
+		p[i] = str[i];
+	return (p);
+}
+
 /**
  *new_dog - function that creates a new dog.
  *
@@ -13,19 +41,19 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	char *c = name;
-	char *b = owner;
 
-	if (name == NULL)
-		c = "(nil)";
-	if (owner == NULL)
-		b = "(nil)";
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = c;
+	dog->name = _strdup(name);
 	dog->age = age;
-	dog->owner = b;
-	return (dog);
+	dog->owner = _strdup(owner);
+	if (!dog->owner || !dog->name)
+	{
+		free(dog->name);
+		free(dog->owner);
+		free(dog);
+	}
 
+	return (dog);
 }
