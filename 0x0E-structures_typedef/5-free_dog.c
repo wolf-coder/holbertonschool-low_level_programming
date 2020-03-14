7b1f01a811de50ca9920832df8de0c4ba50a4b66
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-03-14 23:01:32 +0100

    A function that frees dogs.

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ *free_dog - A function that frees dogs.
+ *
+ *@d: Pointer to a type of dog_t-> struct dog.
+ *
+ * Return: Void.
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
