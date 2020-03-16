2722fb62acd70899435e815b72ff999240dff7bb
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-03-16 02:04:16 +0100

    3-op_functions.c file.

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+/**
+ *get_op_func - Get the  appropriate function.
+ *
+ *@s:operand.
+ *
+ *Return: 99 if inappropriate operand. function result on success.
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+	int i = 0;
+
+	while (ops[i].op)
+	{
+		if (ops[i].op[0] == s[0])
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	printf("Error\n");
+	exit(99);
+}
