90a6eade0a123d793cd3c3ade14b996932d69172
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-03-17 21:13:42 +0100

    fixing output.

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,10 +1,10 @@
 section .data
-    message db "Hello, Holberton", 10
+    str db "Hello, Holberton", 10
 section .text
 global main
 main:
     mov rax, 1
     mov rdi, 1
-    mov rsi, message
+    mov rsi, str
     mov rdx, 17
     syscall
