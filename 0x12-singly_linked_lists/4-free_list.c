e5538b1bd3b27908c6152e500d89419bc5cbb8a2
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-03-22 23:57:59 +0100

    output fix

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <stdio.h>
 #include "stdlib.h"
+#include <unistd.h>
 /**
 * free_list -A function that frees a list_t list.
 * @head: Pointer to the head element of a list.
