d9c20b2cb032c5314b497ebb70fdc850e1bf770f
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-03-23 23:20:47 +0100

    header file.

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef MORE_LINKED
+#define MORE_LINKED
+
+#include <stddef.h>
+int _putchar(char c);
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+size_t print_listint(const listint_t *h);
+
+#endif
