5cf082633ed172082e14f189a5865839e5433358
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-03-24 21:13:05 +0100

    Adding add_nodeint() prototype.

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,8 @@
 #ifndef MORE_LINKED
 #define MORE_LINKED
-
 #include <stddef.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 /**
  * struct listint_s - singly linked list
@@ -16,6 +17,9 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+
 #endif
