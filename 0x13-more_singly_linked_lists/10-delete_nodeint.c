3ad1d9337f6f9818a69ef974fe109df8fef8cf2f
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-03-27 21:54:22 +0100

    Adding pop_listint() function.

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,6 +19,28 @@ size_t listint_len(const listint_t *h)
 	}
 	return (sum);
 }
+/**
+ *pop_listint - A function that deletes the head node of a
+ *listint_t linked list, and returns the head node’s data (n).
+ *
+ *@head: Pointer to the head node of a given list.
+ *
+ *Return: The n's data head of the updated list.
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp_node = NULL;
+	int n;
+
+	tmp_node = *head;
+	if (!tmp_node)
+		return (0);
+	*head = tmp_node->next;
+	n = tmp_node->n;
+	free(tmp_node);
+	return (n);
+}
 
 /**
  *delete_nodeint_at_index- a function that deletes the node at index
