238d8171702f3e7e829a0557540fd90bda4c953f
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-03-27 21:49:23 +0100

    A  function that deletes the node at index index of a listint_t linked list.

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,59 @@
+#include "lists.h"
+
+/**
+ *listint_len - A function that returns the number of elements in a
+ *linked listint_t list.
+ *
+ *@h: Pointer to the head of a given list.
+ *
+ *Return: Number of 'h' nodes elements.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t sum = 0;
+
+	while (h)
+	{
+		sum++;
+		h = h->next;
+	}
+	return (sum);
+}
+
+/**
+ *delete_nodeint_at_index- a function that deletes the node at index
+ *index of a listint_t linked list.
+ *
+ *@index: The index of the node that should be deleted. Index starts at 0
+ *@head: Double Pointer to the head's node of a given list.
+ *
+ *Return: 1 if it succeeded, -1 if it failed.
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int len = listint_len(*head), i = 0;
+	listint_t *Tmp_node = *head, *Node_del = NULL;
+
+	if (index > len - 1)
+		return (-1);
+	if (index == 0)
+	{
+		pop_listint(head);
+		return (-1);
+	}
+	if (index == len - 1)
+	{
+		while (Tmp_node->next->next)
+			Tmp_node = Tmp_node->next;
+		Node_del = Tmp_node->next;
+		Tmp_node->next = NULL;
+		return (-1);
+	}
+	while (i++ < index - 1)
+		Tmp_node = Tmp_node->next;
+	Node_del = Tmp_node->next;
+	Tmp_node->next = Node_del->next;
+	free(Node_del);
+	return (1);
+}
