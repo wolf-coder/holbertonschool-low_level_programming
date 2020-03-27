22b99492a0e3acc876779eb2a3ab0a3790ee3065
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-03-27 22:27:40 +0100

    Fixing NULL case.

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -57,9 +57,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int len = listint_len(*head), i = 0;
 	listint_t *Tmp_node = *head, *Node_del = NULL;
 
-	if (head == NULL)
-		return;
-	if (index > len - 1)
+	if (!head || index > len - 1)
 		return (-1);
 	if (index == 0)
 	{
