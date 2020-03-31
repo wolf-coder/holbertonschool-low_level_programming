300fecdfb19f1578b47884e0be00219faf13b4ca
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-03-31 02:30:55 +0100

    Adding print_binary() prototype.

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,5 +1,6 @@
 #ifndef BIT_MNIPULATION
 #define BIT_MNIPULATION
+
 #include <stdio.h>
 #include <stdlib.h>
 int _putchar(char c);
@@ -7,4 +8,6 @@ int _strlen(char *s);
 unsigned int binary_to_uint(const char *b);
 unsigned int _pow_to_2(unsigned int n);
 char *_strdup(const char *str);
+void print_binary(unsigned long int n);
+
 #endif
