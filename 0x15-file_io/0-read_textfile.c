12370ba498df14638bf865f8deb6f1e85fce371d
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-04-01 23:53:58 +0100

    deleting putchar() func

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,15 +1,4 @@
 #include "holberton.h"
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
 /**
  *read_textfile- A function that reads a text file and prints it
