0c7a03465a9d711a61dc1368b94655b854e03281
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-04-01 23:56:53 +0100

    fixing betty style

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,13 +25,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 	buff = malloc(sizeof(char) * letters);
-    	readen_bytes = read(fd, buff, letters);
-	if(readen_bytes == -1)
-		return(0);
-	written_bytes = write(STDOUT_FILENO, buff,readen_bytes);
-	if(written_bytes == -1)
+	readen_bytes = read(fd, buff, letters);
+	if (readen_bytes == -1)
+		return (0);
+	written_bytes = write(STDOUT_FILENO, buff, readen_bytes);
+	if (written_bytes == -1)
 		return (0);
 	free(buff);
 	close(fd);
-	return (written_bytes);
+	return (readen_bytes);
 }
