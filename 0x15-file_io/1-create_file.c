c2101e3095bc3b189a1f22bae60a20581143fc69
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-04-02 21:58:21 +0100

    fixing NULL string case.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t wr;
 
-	if (!filename)
+	if (!filename || !text_content)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
