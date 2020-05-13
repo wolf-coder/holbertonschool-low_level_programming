783f4552d5497a170d9c0e665d33a5740d014410
wolf-coder <cuore.fakhri@gmail.com>
2020-05-13 19:59:22 +0100

    Adding dlistint_len() prototype?

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -13,5 +13,7 @@ typedef struct dlistint_s
     struct dlistint_s *prev;
     struct dlistint_s *next;
 } dlistint_t;
-
+#include <stdio.h>
+#include <stdlib.h>
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
